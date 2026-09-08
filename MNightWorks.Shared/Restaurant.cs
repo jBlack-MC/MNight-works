46d1f3ba66b0affeb54c7ac52ff693d133c10793
jBlack-MC <157570722+jBlack-MC@users.noreply.github.com>
2026-09-08 10:47:31 +0200

    Save changes

---
+++ MNightWorks.Shared/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using MNightWorks.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MNightWorks.Shared
+{
+    public class Restaurant
+    {
+        //its a convenience EF core provides so the c# code ran instead of writing a separate query
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string? Address { get; set; }
+        public List<MenuItem> MenuItems { get; set; } = new(); 
+
+
+    }
+}
